6ec8144c4070b845fee012f9b244009d26fcfacb
chmilevfa <17650184+ChmilevFA@users.noreply.github.com>
2023-01-02 14:20:06 +0100

    devil's advocate template

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,52 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
-  return (
-    <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>sup, insert your headline here</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
-          </div>
+    const [userInput, setUserInput] = useState('');
+    const onUserChangedText = (event) => {
+        setUserInput(event.target.value);
+    };
+    return (
+        <div className="root">
+            <Head>
+                <title>GPT-3 Writer | buildspace</title>
+            </Head>
+            <div className="container">
+                <div className="header">
+                    <div className="header-title">
+                        <h1>Feedback on your project ideas from AI!</h1>
+                    </div>
+                    <div className="header-subtitle">
+                        <h2>Put your project idea into the input field and the AI will give you feedback and spot any potential issues.</h2>
+                    </div>
+                </div>
+                <div className="prompt-container">
+                    <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText}/>
+                </div>
+                <div className="prompt-buttons">
+                    <a className="generate-button" onClick={null}>
+                        <div className="generate">
+                            <p>Generate</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div className="badge-container grow">
+                <a
+                    href="https://buildspace.so/builds/ai-writer"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <div className="badge">
+                        <Image src={buildspaceLogo} alt="buildspace logo"/>
+                        <p>build with buildspace</p>
+                    </div>
+                </a>
+            </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
